f2e000a4be1ef6d8151cc18b65dce12481dd657d
kyungeun <jjaaaaaang22@gmail.com>
2021-09-08 03:04:37 +0900

    Update faq-accordion-card

--- faq-accordion-card/index.js
+++ faq-accordion-card/index.js
@@ -1,14 +1,25 @@
-let faqDrop = document.getElementsByClassName('drop-title');
-console.log(faqDrop)
+
+let faqDrop = document.getElementsByClassName('drop-down')[0].children;
 
 for (let i=0; i<faqDrop.length; i++) {
     faqDrop[i].addEventListener('click', () => {
-        openFaqDrop(faqDrop[i].children)
+        var fd = faqDrop[i].children
+        openFaqDrop(fd)
+        fd = null
     });
 }
 
+let section = document.querySelector('section')
+window.getComputedStyle(section).getPropertyValue('height')
+
+
 function openFaqDrop(fd) {
     if (fd[1].classList[1] === 'close') {
+        let section = document.querySelector('section');
+        // let sec_height = window.getComputedStyle(section).getPropertyValue('height');
+        let sec_height = window.getComputedStyle(section).getPropertyValue('height');
+        document.querySelector('section').style.height = 'calc(${sec_height} + 2.2rem)';
+        console.log(window.getComputedStyle(section).getPropertyValue('height'))
         fd[1].classList.remove('close')
         fd[1].classList.add('open')
         fd[0].style.fontWeight = '700'
@@ -17,4 +28,5 @@ function openFaqDrop(fd) {
         fd[1].classList.add('close')
         fd[0].style.fontWeight = '400'
     }
+     
 }
