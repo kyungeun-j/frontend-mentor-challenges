a1695f71863de6220e6c3ae8b4c4be576f29214e
kyungeun <jjaaaaaang22@gmail.com>
2021-10-04 20:12:43 +0900

    Update faq-accordion-card

--- faq-accordion-card/index.js
+++ faq-accordion-card/index.js
@@ -1,4 +1,3 @@
-
 let faqDrop = document.getElementsByClassName('drop-down')[0].children;
 
 for (let i=0; i<faqDrop.length; i++) {
@@ -9,24 +8,16 @@ for (let i=0; i<faqDrop.length; i++) {
     });
 }
 
-let section = document.querySelector('section')
-window.getComputedStyle(section).getPropertyValue('height')
-
-
 function openFaqDrop(fd) {
     if (fd[1].classList[1] === 'close') {
-        let section = document.querySelector('section');
-        // let sec_height = window.getComputedStyle(section).getPropertyValue('height');
-        let sec_height = window.getComputedStyle(section).getPropertyValue('height');
-        document.querySelector('section').style.height = 'calc(${sec_height} + 2.2rem)';
-        console.log(window.getComputedStyle(section).getPropertyValue('height'))
         fd[1].classList.remove('close')
         fd[1].classList.add('open')
         fd[0].style.fontWeight = '700'
+        fd[0].children[0].style.transform = 'rotate(180deg)'
     } else {
         fd[1].classList.remove('open')
         fd[1].classList.add('close')
         fd[0].style.fontWeight = '400'
+        fd[0].children[0].style.transform = 'rotate(0deg)'
     }
-     
 }
